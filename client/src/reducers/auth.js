72ef64493eb2839a398cd722b3074c6641703df8
michalosman <michal.osman.997@gmail.com>
2021-12-07 15:09:26 +0100

    Add client side Auth

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,25 @@
+import {
+  SIGN_UP,
+  SIGN_IN,
+  SIGN_OUT,
+  AUTO_SIGN_IN,
+} from '../constants/actionTypes'
+
+const authReducer = (auth = null, action) => {
+  switch (action.type) {
+    case SIGN_UP:
+    case SIGN_IN:
+      if (!action.payload.token) return null
+      localStorage.setItem('userData', JSON.stringify(action.payload))
+      return action.payload
+    case SIGN_OUT:
+      localStorage.clear()
+      return null
+    case AUTO_SIGN_IN:
+      return JSON.parse(localStorage.getItem('userData'))
+    default:
+      return auth
+  }
+}
+
+export default authReducer
---
+++ client/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import authReducer from './auth'
+
+const rootReducer = combineReducers({ auth: authReducer })
+
+export default rootReducer
