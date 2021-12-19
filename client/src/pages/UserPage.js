1403eeccf578c235a95adf98e628940b89d42dd1
michalosman <michal.osman.997@gmail.com>
2021-12-19 03:37:10 +0100

    Use routing path instead of location

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -2,14 +2,24 @@ import React from 'react'
 import { Box } from '@mui/material'
 import Sidebar from '../components/Sidebar'
 import Chat from '../components/Chat'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const UserPage = () => {
   return (
     <Router>
       <Box display="flex" height="100vh">
-        <Sidebar />
-        <Chat />
+        <Routes>
+          <Route path="/" element={<Sidebar />} />
+          <Route
+            path=":chatId"
+            element={
+              <>
+                <Sidebar />
+                <Chat />
+              </>
+            }
+          />
+        </Routes>
       </Box>
     </Router>
   )
