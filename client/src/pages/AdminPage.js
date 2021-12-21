224b69034c09d1ec572e8c5ed5bdf81115c96825
michalosman <michal.osman.997@gmail.com>
2021-12-21 05:37:28 +0100

    Refactor Admin Page

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { signOut } from '../actions/auth'
 import { useDispatch } from 'react-redux'
-import { Box, Button, IconButton } from '@mui/material'
-import { fetchUsers, warnUser, blockUser } from '../api'
-import WarningIcon from '@mui/icons-material/Warning'
+import { Box, Button, IconButton, Typography } from '@mui/material'
+import { fetchUsers, blockUser } from '../api'
 import BlockIcon from '@mui/icons-material/Block'
-import { v4 as uuidv4 } from 'uuid'
 
 const AdminPage = () => {
   const dispatch = useDispatch()
@@ -20,27 +18,42 @@ const AdminPage = () => {
     setUsers(data)
   }
 
-  const userBoxes = users.map((user) => (
-    <Box
-      key={uuidv4()}
-      display="flex"
-      alignItems="center"
-      justifyContent="space-between"
-      p={2}
-      mb={2}
-      border={1}
-    >
-      {user.name}
-      <Box ml={4}>
-        <IconButton onClick={() => warnUser(user._id)}>
-          <WarningIcon />
-        </IconButton>
-        <IconButton onClick={() => blockUser(user._id)}>
-          <BlockIcon />
-        </IconButton>
+  const handleBlock = async (userId) => {
+    await blockUser(userId)
+    window.location.reload(false)
+  }
+
+  const userBoxes = users.map((user) =>
+    user.warningsCount >= 3 && !user.isBlocked ? (
+      <Box
+        key={user._id}
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        p={2}
+        mb={2}
+        borderRadius="10px"
+        boxShadow={3}
+        style={{ width: 500 }}
+      >
+        <Typography>
+          <strong>User:</strong> {user.name}
+        </Typography>
+        <Typography>
+          <strong>Warnings:</strong> {user.warningsCount}
+        </Typography>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => handleBlock(user._id)}
+        >
+          Block
+        </Button>
       </Box>
-    </Box>
-  ))
+    ) : (
+      ''
+    )
+  )
 
   return (
     <Box
