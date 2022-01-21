cf779649f109333a69988e1e52fc587cc8b6af54
michalosman <michal.osman.997@gmail.com>
2022-01-21 22:48:41 +0100

    Clean code

--- server-sql/controllers/userController.ts
+++ server-sql/controllers/userController.ts
@@ -19,11 +19,11 @@ export const signUp = async (req: Request, res: Response) => {
 
   const hashedPassword = await bcrypt.hash(password, 12)
 
-  const newUser = await User.create({
-    email,
-    password: hashedPassword,
-    name,
-  }).save()
+  const newUser = new User()
+  newUser.email = email
+  newUser.password = hashedPassword
+  newUser.name = name
+  await newUser.save()
 
   const token = jwt.sign({ id: newUser.id }, SECRET_KEY, {
     expiresIn: '7d',
@@ -49,9 +49,9 @@ export const signIn = async (req: Request, res: Response) => {
 }
 
 export const warnUser = async (req: Request, res: Response) => {
-  const userId = parseInt(req.params.userId)
+  const { userId } = req.params
 
-  const user = await User.findOne({ id: userId })
+  const user = await User.findOne(userId)
   if (!user) throw ApiError.notFound('User not found')
 
   user.warnings += 1
@@ -61,9 +61,9 @@ export const warnUser = async (req: Request, res: Response) => {
 }
 
 export const blockUser = async (req: Request, res: Response) => {
-  const userId = parseInt(req.params.userId)
+  const { userId } = req.params
 
-  const user = await User.findOne({ id: userId })
+  const user = await User.findOne(userId)
   if (!user) throw ApiError.notFound('User not found')
 
   user.is_blocked = true
@@ -73,9 +73,9 @@ export const blockUser = async (req: Request, res: Response) => {
 }
 
 export const unblockUser = async (req: Request, res: Response) => {
-  const userId = parseInt(req.params.userId)
+  const { userId } = req.params
 
-  const user = await User.findOne({ id: userId })
+  const user = await User.findOne(userId)
   if (!user) throw ApiError.notFound('User not found')
 
   user.is_blocked = false
@@ -86,18 +86,13 @@ export const unblockUser = async (req: Request, res: Response) => {
 
 export const getUsers = async (req: Request, res: Response) => {
   const users = await User.find()
-  res.status(200).json(
-    users.map((user) => {
-      return {
-        id: user.id,
-        email: user.email,
-        name: user.name,
-        role: user.role,
-        warnings: user.warnings,
-        isBlocked: user.is_blocked,
-      }
-    })
-  )
+
+  const usersData = users.map((user) => {
+    const { password: remove, is_blocked: isBlocked, ...userData } = user
+    return { ...userData, isBlocked }
+  })
+
+  res.status(200).json(usersData)
 }
 
 export const validateUser = (req: Request, res: Response) => {
