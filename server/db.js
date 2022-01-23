6c90dad72b2e6fccdd70cd658b2b94ebb0469082
michalosman <michal.osman.997@gmail.com>
2022-01-23 13:11:54 +0100

    Change _id to id

--- server/db.js
+++ server/db.js
@@ -10,6 +10,12 @@ const connectToDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
+    mongoose.set('toJSON', {
+      virtuals: true,
+      transform: (doc, converted) => {
+        delete converted._id
+      },
+    })
     console.log('Connected to MongoDB')
   } catch (error) {
     console.log(`Unable to connect to MongoDB (${error.message}) `)
